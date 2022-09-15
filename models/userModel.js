a0775e75c25e33ee4a73f041dcb6b35092166886
Ashley <ashleyshi006@gmail.com>
2022-09-15 10:23:08 -0400

    add reset password function

--- models/userModel.js
+++ models/userModel.js
@@ -55,6 +55,13 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000; // Just to ensure that the token is always created after the password has been changed
+  next();
+});
+
 userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
