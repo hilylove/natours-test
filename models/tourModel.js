f2f9d9d3814059ce49407f59f055e03778a9a7d2
Ashley <ashleyshi006@gmail.com>
2022-09-18 17:29:52 -0400

    add geospatial aggregation of calculating distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -173,13 +173,13 @@ tourSchema.post(/^find/, function(docs, next) {
   next();
 });
 
-// AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+// // AGGREGATION MIDDLEWARE
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-  console.log(this.pipeline());
-  next();
-});
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
