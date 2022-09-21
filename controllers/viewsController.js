c69f5cc4ec70f99c566e0e6ce2657c8397cfde53
Ashley <ashleyshi006@gmail.com>
2022-09-21 12:46:01 -0400

    rendering error page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -20,8 +21,11 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user'
   });
 
-  // 2) Build template
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
 
+  // 2) Build template
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
